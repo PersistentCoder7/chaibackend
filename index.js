e315b4bbd8a91a4f44e0ea2dccb983b7b4ecbf4e
Prabhugurudarshan Singh <prabhu25.1975@gmail.com>
2025-02-27 21:59:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 3000;
+const www = process.env.WWW || "./";
+
+/*
+app.use(express.static(www));
+console.log(`serving ${www}`);
+app.get('*', (req, res) => {
+    res.sendFile(`index.html`, { root: www });
+});*/
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("Prabhu Singh");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>Login Page</h1>");
+});
+
+app.get("/youtube", (req, res) => {
+  res.send("<h2>Youtube</h2>");
+});
+
+app.listen(port, () => console.log(`listening on http://localhost:${port}`));
